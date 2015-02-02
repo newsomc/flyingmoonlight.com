d443d2b8aca0e92160ec44de33b06aaad2307c54
Clint Newsom <hcnewsom@gmail.com>
2015-02-02 18:19:03 -0500

    WIP: More fun with the website!

--- source/js/main.js
+++ source/js/main.js
@@ -1,22 +1,34 @@
 $( document ).ready(function() {
+
+  var releaseFormatModal = function(string) {
+    var title = string.charAt(0).toUpperCase() + string.slice(1);
+    return " - " + title;
+  };
   
   var randomPhoto = function() {
     var random = Math.floor(Math.random() * $('.photo-row').length);
     $('.photo-row').hide().eq(random).show();
   };
-
-  console.log(window.location.pathname);
   
   var init = function() {
     randomPhoto();
   };
   
   init();
-
-  $('.selectpicker').selectpicker().change(function () {
-    
-    console.log('SELECT');
+  
+  $('.sidebar-selectpicker').selectpicker().change(function () {
+    var $format = $(this).val(),
+        $price = $(this).find(':selected').data('price');
+    $('.release-title-format').append(releaseFormatModal($format));
+    $('.release-price').html($price);
     $('#release-modal').modal('show');
+  });
+
+  $('.modal-selectpicker').selectpicker().change(function () {
+    var $format = $(this).val(),
+        $price = $(this).find(':selected').data('price');
+    $('.release-title-format').html(releaseFormatModal($format));
+    $('.release-price').html($price);
   });
   
 });
